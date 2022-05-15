1447bc4c0b397d966433164bd33aa91453586aae
donetkit <donetkit@gmail.com>
2022-05-15 16:22:17 +0800

    Trim

--- glog/formatter.go
+++ glog/formatter.go
@@ -108,8 +108,12 @@ func (f *Formatter) Format(entry *logrus.Entry) ([]byte, error) {
 	if !f.NoColors && !f.NoFieldsColors {
 		b.WriteString("\x1b[0m")
 	}
-	entry.Message = strings.Trim(entry.Message, "[")
-	entry.Message = strings.Trim(entry.Message, "]")
+	if strings.HasPrefix(entry.Message, "[[") {
+		entry.Message = entry.Message[2:]
+	}
+	if strings.HasSuffix(entry.Message, "]]") {
+		entry.Message = entry.Message[:len(entry.Message)-2]
+	}
 	// write message
 	if f.TrimMessages {
 		b.WriteString(strings.TrimSpace(entry.Message))
